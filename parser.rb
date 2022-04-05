1eed7f77e0819b87d455b9e75041bd4931b81444
WCanirinka <canirinkawilfried@gmail.com>
2022-04-05 14:42:41 +0200

    Add logic to the parser & Add rspec

---
+++ parser.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+
+require './analyze_parser.rb'
+
+class LogParser
+  attr_accessor :parser
+  def initialize(filepath)
+    self.parser = LogAnalyze.new(filepath)
+  end
+
+  def most_page_views
+    page_views = parser.page_views
+    page_views.map {|path, ip_count| "#{path} - #{ip_count} visits"}.join("\n")
+  end
+
+  def most_unique_page_views
+    unique_views = parser.unique_page_views
+    unique_views.map {|path, uniq_ips| "#{path} - #{uniq_ips} unique views"}.join("\n")
+  end
+
+  def summary
+    ["Most Views:", most_page_views, 
+      "Most unique Views:", most_unique_page_views].join("\n")
+  end
+
+  def print_summary
+    puts summary
+  end
+end
+
+if ARGV.first
+  begin
+    lp = LogParser.new ARGV.first
+    lp.print_summary
+  rescue StandardError => e
+    puts e.message
+  end
+else
+  puts "Usage: ./parser.rb webserver.log"
+end
